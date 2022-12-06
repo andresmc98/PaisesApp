67a08b25b058ef7f316acbb6693840284d28841a
andresmc98 <jossean98@gmail.com>
2022-12-06 14:40:26 -0700

    servicio de busqueda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     PaisModule,
-    SharedModule
+    SharedModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -7,5 +8,23 @@ import { Component } from '@angular/core';
   ]
 })
 export class PorPaisComponent {
+
+  termino: string = 'Hola mundo'
+
+  constructor( private paisService: PaisService){
+
+  }
+
+  buscar(){
+    console.log(this.termino);
+
+    this.paisService.buscarPais( this.termino )
+      .subscribe( resp => {
+        console.log(resp);
+      });
+
+
+
+  }
 
 }

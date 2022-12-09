5c214e6d44d7897bdeddb5cfcb814f2e42cbdab2
andresmc98 <jossean98@gmail.com>
2022-12-09 14:42:50 -0700

    Funcionalidad busqueda de regiones

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
+  styles: [ 
+    `button{
+      margin-right: 5px;
+    }`
   ]
 })
 export class PorRegionComponent {
+
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
+  paises: Country[] = [];
+
+  constructor( private paisService: PaisService){}
+
+  getClaseCSS( region: string):string {
+    return ( region === this.regionActiva) ? 'btn btn-primary' : 'btn btn-outline-primary'
+  }
+
+  activarRegion( region: string) {
+    this.regionActiva = region;
+  }
+
+  buscarRegion (regionActiva:string){
+    this.regionActiva = this.regionActiva;
+    console.log(this.regionActiva);
+
+    this.paisService.buscarRegion( regionActiva )
+      .subscribe( (paises) => {
+        console.log(paises);
+        this.paises = paises;
+        
+      }
+      );
+
+  }
+  
 
 }
